89318b9ded3675ef2e8e93ef374fa43edc38e804
Milan Chalishajarwala <chmilan7@gmail.com>
2020-11-09 12:27:57 -0600

    First commit for Aditya's Branch

--- MovieDB/APIHandler.swift
+++ MovieDB/APIHandler.swift
@@ -45,5 +45,37 @@ class APIHandler{
             }
         }.resume()
     }
+
+func fetchDataForThePurposeOfMergingBranches<T:Decodable>(type: T.Type, contentType: String){
+        let endpoint = "https://api.themoviedb.org/3/movie/\(contentType)?language=en-US&page=undefined&api_key=55957fcf3ba81b137f8fc01ac5a31fb5"
+        
+        let url = URL(string: endpoint)
+        guard let unwrappedUrl = url else { return }
+        URLSession.shared.dataTask(with: unwrappedUrl){
+            (data, response, error) in
+            do{
+                guard let data = data else { return }
+                let jsonData = try JSONDecoder().decode(T.self, from: data)
+                switch(contentType){
+                case "now_playing":
+                    let modelData = jsonData as! MovieModel
+                    if let closureBlock = self.closureForNowShowing {
+                        closureBlock(modelData, response, error)
+                    }
+                    break
+                case "popular":
+                    let modelData = jsonData as! MovieModel
+                    if let closureBlock = self.closureForPopular {
+                        closureBlock(modelData, response, error)
+                    }
+                    break
+                default:
+                    return
+                }
+            }catch{
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
     
 }
